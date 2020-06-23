3501f5a5fdd0847b336c35e92e0c027fe3f597c9
saiprasadmedishetti <saiprasadmedishetti@gmail.com>
2020-06-23 20:28:26 +0530

    added search for description

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -7,8 +7,15 @@ export class SearchPipe implements PipeTransform {
   transform(items: any[], searchText: any): any {
     if (!items) return [];
     if (!searchText) return items;
-    return items.filter((item) =>
-      (item.title || item.name).toLowerCase().includes(searchText.toLowerCase())
-    );
+    // return items.filter((item) =>
+    //   (item.title || item.name).toLowerCase().includes(searchText.toLowerCase())
+    // );
+    return items
+      ? items.filter(
+          (item) =>
+            (item.name || "").search(new RegExp(searchText, "i")) > -1 ||
+            (item.description || "").search(new RegExp(searchText, "i")) > -1
+        )
+      : [];
   }
 }
